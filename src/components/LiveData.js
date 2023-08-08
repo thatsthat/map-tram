4cdc16ee03dae0f1062a59fd90436eb2bfe5efc2
Aram <aram.anto@gmail.com>
2023-08-08 11:37:12 +0200

    correct syntax for api call

--- src/components/LiveData.js
+++ src/components/LiveData.js
@@ -1,14 +1,44 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-const BaseMap = () => {
-  const [counters, setCounters] = useState(0);
+const LiveData = () => {
+  const [info, setInfo] = useState(0);
+
+  const fetchData = async () => {
+    try {
+      const aBody = new URLSearchParams();
+      aBody.append("grant_type", "client_credentials");
+      aBody.append("client_id", process.env.REACT_APP_USER);
+      aBody.append("client_secret", process.env.REACT_APP_SECRET);
+
+      const rawResponse = await fetch(
+        "https://opendata.tram.cat/connect/token",
+        {
+          method: "POST",
+          // prettier-ignore
+          headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: aBody.toString(),
+        }
+      );
+      try {
+        const content = await rawResponse.json();
+        setInfo(content.access_token);
+      } catch (e) {
+        console.log(e);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   useEffect(() => {
-    (() => {})();
+    fetchData();
   }, []);
 
-  return <div>{process.env.REACT_APP_USER}</div>;
+  return <div>{info}</div>;
 };
 
-export default BaseMap;
+export default LiveData;
